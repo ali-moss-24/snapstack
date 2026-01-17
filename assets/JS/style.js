d8abccafa64e04c7aba83a1b35cfcf2de947300a
ali-moss-24 <nodmi6n2@students.codeinstitute.net>
2026-01-17 21:58:42 +0000

    Add alterations to file structure in index.html

--- assets/JS/style.js
+++ assets/JS/style.js
@@ -11,12 +11,27 @@ const topCardSlot = document.getElementById("top-card");
 
 let deck = [];
 let playerHand = [];
-let discardPlie = [];
+let discardPile = [];
+
+function createDeck() {
+    const colors = ["red", "blue", "green", "yellow"];
+    const newDeck = [];
+
+    colors.forEach(color => {
+        for (let i = 0; i <= 9; i++) {
+            newDeck.push({ color: color, number: i });
+        }
+    })
+
+    return newDeck;
+}
+
 
 // EVENT LISTENER
 
 // Draw card button
 drawBtn.addEventListener("click", () => {
+    playerHandArea.innerHTML += "<div class='card red'>5</div>";
     console.log("Draw button clicked");
     // drawCard(); //you'll add this function soon
 });
@@ -29,7 +44,7 @@ function renderHand() {
 }
 
 function updateTopCard() {
-    console.log("Updating top card:", discardPile[discardPile.lengh - 1]);
+    console.log("Updating top card:", discardPile[discardPile.length - 1]);
     //Later: update DOM with the top discard card
 }
 
