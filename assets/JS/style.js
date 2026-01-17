0e8969105fd91763e773ac45cf0d2bab0ba470a3
ali-moss-24 <nodmi6n2@students.codeinstitute.net>
2026-01-17 21:19:43 +0000

    Add styling to style.js

---
+++ assets/JS/style.js
@@ -0,0 +1,48 @@
+//  Draw button
+const drawBtn = document.getElementById("draw-btn");
+
+//Player hand container
+const playerHandArea = document.getElementById("player-hand");
+
+//Discard pile top card
+const topCardSlot = document.getElementById("top-card");
+
+//GAME STATE
+
+let deck = [];
+let playerHand = [];
+let discardPlie = [];
+
+// EVENT LISTENER
+
+// Draw card button
+drawBtn.addEventListener("click", () => {
+    console.log("Draw button clicked");
+    // drawCard(); //you'll add this function soon
+});
+
+// PLACEHOLDER
+
+function renderHand() {
+    console.log("Rendering hand:", playerHand);
+    //Later: update DOM with card elements
+}
+
+function updateTopCard() {
+    console.log("Updating top card:", discardPile[discardPile.lengh - 1]);
+    //Later: update DOM with the top discard card
+}
+
+// INITIAL SETUP
+
+function initGame() {
+    console.log("Game starting.....");
+
+    // Later: create deck, shuffle, start discard pile
+    // createDeck ();
+    // shuffleDeck ();
+    // discardPile.push(deck.pop());
+    //updateTopCard();
+}
+
+initGame();
